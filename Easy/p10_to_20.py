dddecec7c698522185641e269c2a4f47bc924081
Fotouhi Ardakani Mohammadkazem <fotouhiardakanimohammadkazem@johndeere.com>
2022-01-03 22:12:32 -0600

    first try

--- Easy/p10_to_20.py
+++ Easy/p10_to_20.py
@@ -25,9 +25,34 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lower-case English letters."""
 
+
 class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+    def longestCommonPrefix_mkf(self, strs):
+        '''
+        Runtime: 67 ms, faster than 5.11% of Python3 online submissions for Longest Common Prefix.
+        Memory Usage: 14.3 MB, less than 82.17% of Python3 online submissions for Longest Common Prefix.
+        '''
+        if len(strs) == 1:
+            return strs[0]
+        pr = ''
+        for i in range(len(strs[0])):
+            ch = strs[0][i]
+            for j in range(1, len(strs)):
+                if (len(strs[j]) > i):
+                    if (strs[j][i] == ch):
+                        continue
+                    else:
+                        return pr
+                else:
+                    return pr
+            pr += ch
+
+        return pr
+
+sol = Solution()
+out = sol.longestCommonPrefix(["flower", "flow", "flight"])
+print(out)
+out = sol.longestCommonPrefix(['car', ''])
+print(out)
+out = sol.longestCommonPrefix(["flower"])
+print(out)
