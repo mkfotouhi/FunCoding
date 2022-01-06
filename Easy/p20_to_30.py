dad6cdd4de12ce619fb89a6f662950993631e556
Fotouhi Ardakani Mohammadkazem <fotouhiardakanimohammadkazem@johndeere.com>
2022-01-05 19:22:29 -0600

    linked_list class is added

--- Easy/p20_to_30.py
+++ Easy/p20_to_30.py
@@ -90,8 +90,11 @@ The number of nodes in both lists is in the range [0, 50].
 Both list1 and list2 are sorted in non-decreasing order."""
 
 
+# Definition for singly-linked list.
 class ListNode:
-    pass
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
