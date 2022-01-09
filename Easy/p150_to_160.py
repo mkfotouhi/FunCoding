2374eb88e62001a0a4aa77a786d504e68d88739e
Fotouhi Ardakani Mohammadkazem <fotouhiardakanimohammadkazem@johndeere.com>
2022-01-09 14:02:20 -0600

    stack class implementation

--- Easy/p150_to_160.py
+++ Easy/p150_to_160.py
@@ -40,3 +40,46 @@ Constraints:
 Methods pop, top and getMin operations will always be called on non-empty stacks.
 At most 3 * 104 calls will be made to push, pop, top, and getMin.
 """
+
+
+class MinStack:
+    """
+    Runtime: 100 ms, faster than 32.66% of Python3 online submissions for Min Stack.
+    Memory Usage: 18.4 MB, less than 26.42% of Python3 online submissions for Min Stack.
+    """
+    def __init__(self):
+        self.vals = dict()
+        self.min = None
+        self.len = 0
+
+    def push(self, val: int) -> None:
+        self.vals[self.len] = val
+        self.len += 1
+        if self.min is not None:
+            self.min = min(self.min, val)
+        else:
+            self.min = val
+
+    def pop(self) -> None:
+        self.len -= 1
+        out = self.vals[self.len]
+        del self.vals[self.len]
+        if out == self.min:
+            if len(self.vals) > 0:
+                self.min = min(list(self.vals.values()))
+            else:
+                self.min = None
+
+    def top(self) -> int:
+        return self.vals[self.len - 1]
+
+    def getMin(self) -> int:
+        return self.min
+
+# Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(-3)
+obj.push(1)
+obj.pop()
+param_3 = obj.top()
+param_4 = obj.getMin()
