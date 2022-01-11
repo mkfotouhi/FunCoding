ff79d4485cc654e0bf7f35c281239fe086e6b360
fotouhif <fotouhif.93@gmail.com>
2022-01-11 16:34:30 -0600

    My solution

--- Easy/p100-to-p110.py
+++ Easy/p100-to-p110.py
@@ -1,3 +1,6 @@
+# *******************************************************
+# ************  problem 7  (100 on LeetCode)  *************
+# *******************************************************
 """
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.
 
@@ -30,10 +33,29 @@ range[0, 100].
 -104 <= Node.val <= 104 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-#class Solution:
-#    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+p = TreeNode(1)
+p_left = TreeNode(2)
+p.left = p_left
+
+q = TreeNode(1)
+q_left = TreeNode(1)
+q.left = q_left
+
+def isSameTree_Fati(p, q) -> bool:
+    flag = True
+    if p and q and p.val == q.val:
+        flag = isSameTree_Fati(p.left, q.left)
+        if flag:
+            flag = isSameTree_Fati(p.right, q.right)
+    elif ((not q) and p) or ((not p) and q):
+        flag = False
+    elif p and q and p.val != q.val:
+        flag = False
+    return flag
+print(isSameTree_Fati(p,q))
