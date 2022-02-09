b92201adb310403b317af866ba9a900d8abc650e
Fotouhi Ardakani Mohammadkazem <fotouhiardakanimohammadkazem@johndeere.com>
2022-02-08 23:50:52 -0600

    meta problem

--- Medium/Meta_2.py
+++ Medium/Meta_2.py
@@ -201,3 +201,59 @@ def minWindow_2(s: str, t: str) -> str:
 
 print(minWindow_2('aaabbbbcaccdd', 'abcdd'))
 print('jhfk')
+
+
+# *******************************************************
+# ************  Leetcode 26   *************
+# *******************************************************
+"""
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)"""
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    """
+    Runtime: 380 ms, faster than 27.56% of Python3 online submissions for Product of Array Except Self.
+    Memory Usage: 29.7 MB, less than 5.22% of Python3 online submissions for Product of Array Except Self.
+    O(n) time and space complexity.
+    """
+    dict_rl = dict()
+    dict_lr = dict()
+    for i in range(len(nums)):
+        if i == 0:
+            dict_rl[i] = 1
+        else:
+            dict_rl[i] = dict_rl.get(i - 1, 1) * nums[i - 1]
+    for i in range(len(nums) - 1, -1, -1):
+        if i == len(nums) - 1:
+            dict_lr[i] = 1
+        else:
+            dict_lr[i] = dict_lr.get(i + 1, 1) * nums[i + 1]
+
+    out = []
+    for i in range(len(nums)):
+        out.append(dict_rl[i] * dict_lr[i])
+    return out
